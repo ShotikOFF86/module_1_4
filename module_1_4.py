4416a09a970b229368951d30fb2c25edb9e37720
ShotikOFF86 <ledidog2000@gmail.com>
2024-10-08 15:06:10 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+city = input("Введите город: ")
+print("Ваш город, ",city.upper())
+current_yеar = 2024
+year_of_foundation = int(input("Год основания Вашего города "))
+age_city = current_yеar - year_of_foundation
+print("В этом году Вашему городу ", age_city, "лет")
+print("Колличество символов в названии города",len(city))
